28b2da90dc8d54d07f8fc0c7ad0c7e62adf61b47
togashi_chihiro <togashi.chihiro@alhinc.jp>
2025-02-06 00:49:31 +0900

    e_if_for までほぼおさらい（eの5－5はコメントかけてないのでもしあれば。）

--- src/e_if_for/Answer05_04.java
+++ src/e_if_for/Answer05_04.java
@@ -34,3 +34,32 @@ public class Answer05_04 {
 	}
 
 }
+
+
+/*
+始め（i=0番目の時）に最大値と最小値をセットしないと後が狂う。（0よりちっちゃい数字とか無いので。）
+以下、私が陥ったミスコードをば
+
+public class Question05_04 {
+
+	public static void main(String[] args) {
+		//自然数（正の整数）(任意)
+		int[] num = { 1, 9, 93, 2, 11, 76, 9, 66, 44, 33 };
+		int maxValue = 0;
+		int minValue = 0;
+
+		//処理
+		for(int i = 0; i < 10; i++) {
+				if(num[i] > maxValue) {
+					maxValue = num[i];
+				}if(num[i] < minValue) {
+					minValue = num[i];
+				}
+		}
+		System.out.println("最大値=" + maxValue + " 最小値=" + minValue);
+	}
+
+}
+
+ちなみにこれだとコンソールには　最大値=93 最小値=0　と出る。
+*/
